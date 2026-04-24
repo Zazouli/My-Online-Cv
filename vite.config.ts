a57567e7c7ffd059d1363891229a05222c6aed6c
Zazouli <oussamazazouli@gmail.com>
2026-04-24 18:33:17 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // Ensures relative paths for static hosting
+    base: '/My-Online-Cv/', // Ensures relative paths for static hosting
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
